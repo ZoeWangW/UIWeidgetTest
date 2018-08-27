1c73e4c058086c3d5e2f877efb902b4bfaafa133
ZoeWangW <13438165026@163.com>
2018-08-27 18:35:22 +0800

    设置进度条的可见性visibility
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/zoe/uiwidgettest/MainActivity.java
@@ -6,18 +6,24 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private Button button;
     private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         button.setOnClickListener(this);
     }
 
@@ -25,8 +31,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v){
         switch (v.getId()){
             case R.id.button:
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
+                if (progressBar.getVisibility() == View.GONE){
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+                else {
+                    progressBar.setVisibility(View.GONE);
+                }
                 break;
             default:
                 break;
